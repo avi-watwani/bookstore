aee2b0d3bef51f5b4e9d6df36800ef9e9eb88b24
Avi Watwani <aviwatwani@Avis-MacBook-Pro.local>
2022-07-06 19:19:28 +0530

    user can switch locale

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,6 +5,7 @@ class StoreController < ApplicationController
   
   def index
     if params[:set_locale]
+      session[:locale] = params[:set_locale]
       redirect_to store_index_url(locale: params[:set_locale])
     else
       @products = Product.order(:title)
