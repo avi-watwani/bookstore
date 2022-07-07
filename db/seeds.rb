afc6b2fdd6e46c69ac886a2e67034a3ad0dabd4d
Avi Watwani <aviwatwani@Avis-MacBook-Pro.local>
2022-07-07 11:52:24 +0530

    added prods

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,94 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.delete_all
 # . . .
-Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
-description:
-%{<p>
-<em>Native Apps, Multiple Platforms</em>
-Answer the question “Can we build this for ALL the devices?” with a
-resounding YES. This book will help you get there with a real-world
-introduction to seven platforms, whether you’re new to mobile or an
-experienced developer needing to expand your options. Plus, you’ll find
-out which cross-platform solution makes the most sense for your needs.
-</p>},
-image_url: '7apps.jpg',
-price: 26.00)
-# . . .
+Product.create([
+    {
+        title: 'Seven Mobile Apps in Seven Weeks',
+        description:
+        %{<p>
+        <em>Native Apps, Multiple Platforms</em>
+        Answer the question “Can we build this for ALL the devices?” with a
+        resounding YES. This book will help you get there with a real-world
+        introduction to seven platforms, whether you’re new to mobile or an
+        experienced developer needing to expand your options. Plus, you’ll find
+        out which cross-platform solution makes the most sense for your needs.
+        </p>},
+        image_url: '7apps.jpg',
+        price: 26.00
+    },
+
+    {
+        title: 'Atomic Habits',
+        description:
+        %{<p>
+        <em>Native Apps, Multiple Platforms</em>
+        Answer the question “Can we build this for ALL the devices?” with a
+        resounding YES. This book will help you get there with a real-world
+        introduction to seven platforms, whether you’re new to mobile or an
+        experienced developer needing to expand your options. Plus, you’ll find
+        out which cross-platform solution makes the most sense for your needs.
+        </p>},
+        image_url: '7apps.jpg',
+        price: 26.00
+    },
+
+    {
+        title: 'Zero to One',
+        description:
+        %{<p>
+        <em>Native Apps, Multiple Platforms</em>
+        Answer the question “Can we build this for ALL the devices?” with a
+        resounding YES. This book will help you get there with a real-world
+        introduction to seven platforms, whether you’re new to mobile or an
+        experienced developer needing to expand your options. Plus, you’ll find
+        out which cross-platform solution makes the most sense for your needs.
+        </p>},
+        image_url: '7apps.jpg',
+        price: 26.00
+    },
+
+    {
+        title: 'Will',
+        description:
+        %{<p>
+        <em>Native Apps, Multiple Platforms</em>
+        Answer the question “Can we build this for ALL the devices?” with a
+        resounding YES. This book will help you get there with a real-world
+        introduction to seven platforms, whether you’re new to mobile or an
+        experienced developer needing to expand your options. Plus, you’ll find
+        out which cross-platform solution makes the most sense for your needs.
+        </p>},
+        image_url: '7apps.jpg',
+        price: 26.00
+    },
+
+    {
+        title: 'The Psychology of Money',
+        description:
+        %{<p>
+        <em>Native Apps, Multiple Platforms</em>
+        Answer the question “Can we build this for ALL the devices?” with a
+        resounding YES. This book will help you get there with a real-world
+        introduction to seven platforms, whether you’re new to mobile or an
+        experienced developer needing to expand your options. Plus, you’ll find
+        out which cross-platform solution makes the most sense for your needs.
+        </p>},
+        image_url: '7apps.jpg',
+        price: 26.00
+    },
+
+    {
+        title: 'Rich Dad Poor Dad',
+        description:
+        %{<p>
+        <em>Native Apps, Multiple Platforms</em>
+        Answer the question “Can we build this for ALL the devices?” with a
+        resounding YES. This book will help you get there with a real-world
+        introduction to seven platforms, whether you’re new to mobile or an
+        experienced developer needing to expand your options. Plus, you’ll find
+        out which cross-platform solution makes the most sense for your needs.
+        </p>},
+        image_url: '7apps.jpg',
+        price: 26.00
+    }
+])
